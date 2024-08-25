f5f560b03347807dbef4d9e0cd6e4d27d77cb23d
kotoshin <yspm1127@gmail.com>
2024-08-25 22:29:32 +0900

    レッスン18

---
+++ src/test/java/com/techacademy/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.techacademy.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryTest {
+
+    @Test
+    void testFindById() {
+        fail("まだ実装されていません");
+    }
+
+}
